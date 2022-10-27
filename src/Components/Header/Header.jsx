83d64beee22b2914e05639da059284939dc0cd1a
O-Julia-O <julia.qq.onix@gmail.com>
2022-10-27 22:15:42 +0300

    New changes

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
+import { useTelegram } from '../../hooks/useTelegram';
 
 const Header = (props) => {
-    
-const tg = window.Telegram.WebApp;
-
-    const onClose = () => {
-        tg.close();
-      }
+    const {user, onClose} = useTelegram();
 
     return (
         <div className={'header'}>
             <Button onClick={onClose}>Close!</Button>
-            <span className={'username'}>{tg.initDataUnsafe?.user?.username}</span>
+            <span className={'username'}>
+                {user?.username}
+                </span>
         </div>
     );
 };
