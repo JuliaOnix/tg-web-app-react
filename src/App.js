6b628ed439aa6e24c4cffc2cc514a34347864a03
O-Julia-O <julia.qq.onix@gmail.com>
2022-11-03 01:40:44 +0200

    f

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       <Header />
       <Routes>
         <Route index element={<ProductList></ProductList>}/>
-        <Route path='form' element={<Form></Form>}/>
+        <Route path={'form'} element={<Form></Form>}/>
       </Routes>
     </div>
   );
