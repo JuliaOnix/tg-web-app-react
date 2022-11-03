1554047c68d6ad87f82e7dbbe5dad1713bd81716
O-Julia-O <julia.qq.onix@gmail.com>
2022-11-03 02:47:20 +0200

    ddddd

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -10,6 +10,28 @@ const Form = () => {
     const [subject, setSubject] = useState('physical');
     const {tg} = useTelegram();
 
+    const onSendData = useCallback(
+      () => {
+        const data = {
+          country,
+          street,
+          subject
+        }
+
+        tg.sendData(JSON.stringify(data))
+      },
+      [],
+    )
+    
+
+    useEffect(() => {
+      tg.onEvent("mainButtonClicked", onSendData)
+
+      return () => {
+        tg.offEvent("mainButtonClicked", onSendData)
+      }
+    }, [])
+
     useEffect(() => {
         tg.MainButton.setParams({
           text: 'Send data'
